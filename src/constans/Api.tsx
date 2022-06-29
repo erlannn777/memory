b255e9cb2975cbb6eeff7409ec36c225522a3eeb
erlannn777 <erlank123456789@gmail.com>
2022-06-29 19:35:35 +0600

    login

---
+++ src/constans/Api.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const API = "https://memoriesmanager.site/AdminPanel/Libs";
+
+const $api = axios.create({
+  baseURL: API,
+});
+
+export const publicApi = axios.create({
+  baseURL: API,
+});
+
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
+
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (err) => {
+    const originalRequest = err.config;
+    if (err.response.status === 401 && !err.config._isRetry) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.post(`${API}/api/v1/users/refresh/`, {
+          refresh: localStorage.getItem("refresh"),
+        });
+        localStorage.setItem("token", response.data.access);
+        return $api.request(originalRequest);
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    } else {
+      return Promise.reject(err);
+    }
+  }
+);
+
+export default $api;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import $api from "../../constans/Api";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(email);
+  console.log(password);
+
+  const handleSubmit = async () => {
+    try {
+      const res = $api.post("/authorisation.php", {
+        login: email,
+        password: password,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="w-full min-h-screen bg-slate-100 flex justify-center">
       <div className="mx-36 w-full h-96 flex flex-col mt-8 items-center  rounded-3xl bg-white p-14">
@@ -9,6 +26,8 @@ const Login = () => {
           Введите логин и пароль{" "}
         </span>
         <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="mt-6 border rounded-3xl h-10 p-2 bg-slate-50"
           placeholder="email"
           type="text"
@@ -16,13 +35,18 @@ const Login = () => {
           id=""
         />
         <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="mt-6 border rounded-3xl h-10 p-2 bg-slate-50"
           placeholder="password"
           type="text"
           name=""
           id=""
         />
-        <button className="mt-12 rounded-3xl py-2 px-12 text-white bg-sky-700">
+        <button
+          onClick={() => handleSubmit()}
+          className="mt-12 rounded-3xl py-2 px-12 text-white bg-sky-700"
+        >
           Войти
         </button>
       </div>
